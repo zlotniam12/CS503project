587906ab0a880da086610462cca4930dea307690
zlotniam12 <zlotniam12@bonaventure.edu>
2017-12-11 14:30:32 -0800

    final database changes

--- mysqlphpsrvr/start.sh
+++ mysqlphpsrvr/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -it -p 8080:80 -p 8081:3306 -v $PWD/mysql:/tmp/mysql -v $PWD/php:/var/www/html talshmaya/cs503
+sudo docker run -it -p 8080:80 -p 8081:3306 -v $PWD/mysql:/tmp/mysql -v $PWD/php:/var/www/html zalyssa/cs503
